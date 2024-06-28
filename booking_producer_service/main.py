51e2cef3c05480ad69808bc5eaf522dfb6d6ae2a
thomas-chiang <thomas801014@gmail.com>
2024-06-28 10:28:18 +0800

    feat: uuid

--- booking_producer_service/main.py
+++ booking_producer_service/main.py
@@ -5,13 +5,14 @@ import aio_pika
 import asyncio
 import aiohttp
 import os
+import sys
+import uuid
 
 app = FastAPI()
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
-RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT", 5672))
 BOOKING_CONSUMER_URL = os.getenv("BOOKING_CONSUMER_URL", "http://localhost:8001/")
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
@@ -24,29 +25,40 @@ class BookingRequest(BaseModel):
 async def create_booking(request: BookingRequest):
     flight_id = request.flight_id
     customer_id = request.customer_id
+    booking_id = str(uuid.uuid4())
 
     await asyncio.gather(
-        send_to_queue(flight_id, customer_id),
-        check_and_update_redis(flight_id)
+        send_to_queue(flight_id, customer_id, booking_id),
+        initialize_consumer(flight_id)
     )
 
-    return {"status": "booking request processed"}
+    return {"status": "booking request processed", "booking_id": booking_id}
 
-async def send_to_queue(flight_id, customer_id):
+async def send_to_queue(flight_id, customer_id, booking_id):
     connection = await aio_pika.connect_robust(f"amqp://guest:guest@{RABBITMQ_HOST}/")
     async with connection:
         channel = await connection.channel()
         exchange = await channel.declare_exchange(flight_id, aio_pika.ExchangeType.TOPIC)
-        message = aio_pika.Message(body=customer_id.encode())
-        print(customer_id, message)
+        message_body = customer_id + "_" + booking_id
+        message = aio_pika.Message(body=message_body.encode())
         await exchange.publish(message, routing_key=flight_id)
 
-async def check_and_update_redis(flight_id):
-    if not await r.exists(flight_id):
+async def initialize_consumer(flight_id):
+    timeout = 10
+    start_time = asyncio.get_event_loop().time()
+    
+    while not await r.exists(flight_id):
+        if asyncio.get_event_loop().time() - start_time > timeout:
+            print(f"Timeout reached: {timeout} seconds")
+            sys.stdout.flush()
+            break
+
         async with aiohttp.ClientSession() as session:
-            try: 
-                async with session.post(BOOKING_CONSUMER_URL, json={"flight_id": flight_id}) as response:
-                    if response.status == 200:
-                        await r.set(flight_id, 1)
+            try:
+                async with session.post(BOOKING_CONSUMER_URL, json={"flight_id": flight_id}):
+                    pass
             except Exception as e:
                 print(e)
+                sys.stdout.flush()
+        
+        await asyncio.sleep(1)
